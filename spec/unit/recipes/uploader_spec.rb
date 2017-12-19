2914ca4f6f32702d6128d27ad0dddff6835cc117
David Brown <dmlb2000@gmail.com>
2017-12-19 08:24:15 -0800

    do the rspec stubs

--- spec/unit/recipes/uploader_spec.rb
+++ spec/unit/recipes/uploader_spec.rb
@@ -7,6 +7,11 @@
 require 'spec_helper'
 
 describe 'unit::uploader' do
+  before do
+    stub_command("psql -c '\\l' | grep -q pacifica_metadata").and_return(true)
+    stub_command("psql -c 'SELECT rolname FROM pg_roles;' | grep -q pacifica").and_return(true)
+    stub_command("psql -c '\\l' | grep -q pacifica=").and_return(true)
+  end
   supported_platforms.each do |platform, versions|
     versions.each do |version|
       context "on an #{platform.capitalize}-#{version} box" do
