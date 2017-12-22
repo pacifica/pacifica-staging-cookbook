6de1b3d0c0f7cd5a196b0135fc06574ca2877c59
David Brown <dmlb2000@gmail.com>
2017-12-22 12:02:36 -0800

    try and fix the tests

--- test/integration/reporting/reporting_test.rb
+++ test/integration/reporting/reporting_test.rb
@@ -5,6 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-describe command(%q(cd /var/www/html; METADATA_PORT=tcp://127.0.0.1:8121 POLICY_PORT=tcp://127.0.0.1:8181 PHP_AUTH_USER=dmlb2001 CI_ENV=production php index.php group view)) do
+describe command(%q(/bin/bash -c 'cd /var/www/html; METADATA_PORT=tcp://127.0.0.1:8121 POLICY_PORT=tcp://127.0.0.1:8181 PHP_AUTH_USER=dmlb2001 CI_ENV=production php index.php group view')) do
   its(:exit_status) { should eq 0 }
 end
